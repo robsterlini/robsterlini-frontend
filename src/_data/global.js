96372f1235340ea25e8c1277634f8f9ce1d541cc
Rob Sterlini-Aitchison <github@robsterlini.co.uk>
2020-06-22 19:52:10 +0100

    feat(nav): Add proper navigation
    Signed-off-by: Rob Sterlini-Aitchison <github@robsterlini.co.uk>

--- src/_data/global.js
+++ src/_data/global.js
@@ -7,5 +7,21 @@ module.exports = () => ({
   social: {
     twitter: 'robsterlini',
     instagram: 'robsterlini',
-  }
+  },
+  nav: [
+    {
+      link: '/journal',
+      label: 'Journal',
+      nav: 'journal',
+    },
+    {
+      link: '/#contact',
+      label: 'Contact',
+    },
+    {
+      link: '/curriculum-vitae',
+      label: '<abbr title="Curriculum Vitae">CV</abbr>',
+      nav: 'cv',
+    },
+  ],
 });
