d4d6561d3860008747daca7b86bf4d258c8ed2cc
Rob Sterlini-Aitchison <github@robsterlini.co.uk>
2020-06-28 21:37:09 +0100

    test(*): Test only inlining crucial instead
    Signed-off-by: Rob Sterlini-Aitchison <github@robsterlini.co.uk>

--- gulpfile.js
+++ gulpfile.js
@@ -5,32 +5,41 @@ var sass = require("gulp-sass");
 
 const { input, output } = require('./config/constants');
 
-gulp.task('separate-scss', function () {
-
-      // using .scss extensions for sass files
-
-  return gulp.src(`src/_includes/**/*.scss`)
-
+gulp.task('scss', function() {
+  return gulp.src(`./${input}/_includes/styles.scss`)
     .pipe(sass({
-      includePaths: [
-        'src/_includes',
-      ],
-    }))
-
-    .pipe(rename(function (file) {
-
-        // file.dirname before any changes
-        console.log("file.dirname  1 = " + file.dirname);
-
-        // // this removes the last directory
-        var temp = file.dirname;
-        // console.log("    temp = " + temp);
-
-        // now add 'Css' to the end of the directory path
-
-        file.dirname = path.join('css', temp);
-        console.log("    after = " + file.dirname);
-    }))
-
-    .pipe(gulp.dest(output));
+      outputStyle: 'compressed'
+    })
+    .on('error', sass.logError))
+    .pipe(gulp.dest(`./${output}`));
 });
+
+// gulp.task('separate-scss', function () {
+
+//       // using .scss extensions for sass files
+
+//   return gulp.src(`src/_includes/**/*.scss`)
+
+//     .pipe(sass({
+//       includePaths: [
+//         'src/_includes',
+//       ],
+//     }))
+
+//     .pipe(rename(function (file) {
+
+//         // file.dirname before any changes
+//         console.log("file.dirname  1 = " + file.dirname);
+
+//         // // this removes the last directory
+//         var temp = file.dirname;
+//         // console.log("    temp = " + temp);
+
+//         // now add 'Css' to the end of the directory path
+
+//         file.dirname = path.join('css', temp);
+//         console.log("    after = " + file.dirname);
+//     }))
+
+//     .pipe(gulp.dest(output));
+// });
