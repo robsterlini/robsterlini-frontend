e41c7ad5623e3cb2e11976db528241c31b376e88
Rob Sterlini-Aitchison <github@robsterlini.co.uk>
2020-07-05 17:05:19 +0100

    feat(journal): Add light/dark mode entry
    Signed-off-by: Rob Sterlini-Aitchison <github@robsterlini.co.uk>

--- src/_data/global.js
+++ src/_data/global.js
@@ -1,9 +1,16 @@
 const baseUrl = 'robsterlini.co.uk';
 
+const {
+  BRANCH: envBranch = 'master',
+} = process.env || {};
+
 module.exports = () => ({
   url: `https://${baseUrl}`,
   name: 'Rob Sterlini',
   email: `hi@${baseUrl}`,
+  github: {
+    branchUrl: `https://github.com/robsterlini/robsterlini-frontend/blob/${envBranch}`,
+  },
   social: {
     twitter: 'robsterlini',
     instagram: 'robsterlini',
