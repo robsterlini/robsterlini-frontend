c33e2018bf2484ce996af6534f48bd20fe1ef0bc
Rob Sterlini <7571609+robsterlini@users.noreply.github.com>
2020-07-16 08:41:11 +0100

    Add nownownow.com page (#30)

---
+++ src/_data/now.js
@@ -0,0 +1,35 @@
+module.exports = () => ({
+  meta: {
+    current: '2020-summer',
+    seasons: {
+      winter: {
+        title: 'Winter',
+      },
+      spring: {
+        title: 'Spring',
+      },
+      summer: {
+        title: 'Summer',
+      },
+      autumn: {
+        title: 'Autumn',
+      },
+    },
+  },
+  years: {
+    '2020': {
+      winter: {
+        life: '<a href="https://www.instagram.com/p/B7rUI5SnNY7/" target="_blank">celebrating Christmas</a> for the first time in a flat that I own, and <a href="https://www.instagram.com/p/B84WwZpnMXI/" target="_blank">skiing with my family</a> in&nbsp;Andorra.',
+        work: 'leveraging Gatsby and Contentful at Fueled to build a new site for&nbsp;<a href="http://svclnk.com/" target="_blank">ServiceLink</a>.',
+      },
+      spring: {
+        life: 'putting my <span class="sc">PADI</span> scuba diving <abbr class="sc" title="open water">OW</abbr> qualification <a href="https://www.instagram.com/p/B7_0MLWn_ii/" target="_blank">into practice</a> in Costa&nbsp;Rica!',
+        work: 'finishing up a new homepage for <a href="https://fueled.com" target="_blank">fueled.com</a>.',
+      },
+      summer: {
+        life: '<a href="https://www.instagram.com/p/CCRJTgoHzml/" target="_blank">discovering trail running</a>, but sadly not on the start line for Ironman&nbsp;UK amid global race cancellations due to Covid-19.',
+        work: 'building web experiences at Fueled for Tony Robbins’ Robbins Research Institute.',
+      },
+    },
+  },
+});
