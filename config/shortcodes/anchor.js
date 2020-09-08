39a11fa70a6dfecef3e92cbc6a7440a77d430d7e
Rob Sterlini <github@robsterlini.co.uk>
2020-09-08 21:59:50 +0100

    feat(*): Finish new font approach
    Signed-off-by: Rob Sterlini <github@robsterlini.co.uk>

--- config/shortcodes/anchor.js
+++ config/shortcodes/anchor.js
@@ -1,5 +1,5 @@
 const anchor = id => {
-  return ` <a href="#${id}" class="anchor">&#182;</a>`;
+  return `<a href="#${id}" class="anchor" title="Jump to heading"><span class="sr-only"> Jump to heading</span></a>`;
 };
 
 module.exports = anchor;
