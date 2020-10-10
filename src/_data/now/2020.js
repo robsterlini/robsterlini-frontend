7dc0d79bfcaa9944280b25b3198d9d53fd22aabe
Rob Sterlini <github@robsterlini.co.uk>
2020-10-10 22:19:34 +0100

    feat(now): Add more now info for the end of 2020
    Signed-off-by: Rob Sterlini <github@robsterlini.co.uk>

---
+++ src/_data/now/2020.js
@@ -0,0 +1,37 @@
+module.exports = {
+  title: 'That year that March was ten months long…',
+  winter: {
+    life: '<a href="https://www.instagram.com/p/B7rUI5SnNY7/" target="_blank">celebrating Christmas</a> for the first time in a flat that I own, and <a href="https://www.instagram.com/p/B84WwZpnMXI/" target="_blank">skiing with my family</a> in&nbsp;Andorra.',
+    work: 'leveraging Gatsby and Contentful at Fueled to build a new site for&nbsp;<a href="http://svclnk.com/" target="_blank">ServiceLink</a>.',
+    music: {
+      artist: 'Rose\xa0Club',
+      link: 'https://open.spotify.com/artist/36d3BDfeOufIRSpy4jLYNA?si=ib8DjtvHRZiQxDouHBOF-g',
+    },
+  },
+  spring: {
+    life: 'putting my <span class="sc">PADI</span> scuba diving <abbr class="sc" title="open water">OW</abbr> qualification <a href="https://www.instagram.com/p/B7_0MLWn_ii/" target="_blank">into practice</a> in Costa&nbsp;Rica!',
+    work: 'finishing up a new homepage experience for visitors to\xa0<a href="https://fueled.com" target="_blank">fueled.com</a>.',
+    music: {
+      artist: 'Kano',
+      title: 'Hoodies All\xa0Summer',
+      link: 'https://open.spotify.com/album/0N6HQ1sWJTf4QJxipeEazS?si=z9uy82NVTSaCTo8V1ccViQ',
+    },
+  },
+  summer: {
+    life: '<a href="https://www.instagram.com/p/CCRJTgoHzml/" target="_blank">discovering trail running</a>, but sadly not on the start line for Ironman&nbsp;UK amid global race cancellations due to&nbsp;<span class="sc">Covid</span>&#8209;19.',
+    work: 'building web experiences at Fueled for Tony\xa0Robbins’ Robbins Research\xa0Institute.',
+    music: {
+      artist: 'Daði\xa0Freyr',
+      title: 'Think About\xa0Things',
+      link: 'https://www.youtube.com/watch?v=VFZNvj-HfBU',
+    },
+  },
+  autumn: {
+    life: 'learning on the job what it means to be <a href="https://www.instagram.com/p/CFVDboLH0ik/" target="_blank">a pug dad</a> with my fawn puppy named\xa0<a href="https://www.instagram.com/frankiedepug/" target="_blank">Frankie</a>.',
+    work: 'refining and iterating on the <abbr title="Robbins Research Institute">RRI</abbr> project through user testing on an alpha\xa0build.',
+    music: {
+      artist: 'Twenty Thousand\xa0Hertz',
+      link: 'https://www.20k.org/',
+    },
+  },
+};
