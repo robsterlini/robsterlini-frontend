1dd3786d9faf9ccb6136a676fbf67c12c0a71db0
Rob Sterlini <7571609+robsterlini@users.noreply.github.com>
2022-05-26 19:12:22 +0100

    May 2022 design updates 🎉 (#75)
    * Design updates 🎉
    
    Signed-off-by: Rob Sterlini <github@robsterlini.co.uk>
    
    * Fix Netlify build issue
    
    Signed-off-by: Rob Sterlini <github@robsterlini.co.uk>
    
    * Switch role=presentation to aria-hidden to fix Netlify a11y
    
    Signed-off-by: Rob Sterlini <github@robsterlini.co.uk>
    
    * Fix #now higlight
    
    Signed-off-by: Rob Sterlini <github@robsterlini.co.uk>
    
    * Fix a11y visibility issues
    
    Signed-off-by: Rob Sterlini <github@robsterlini.co.uk>
    
    * Keep CC and license together
    
    Signed-off-by: Rob Sterlini <github@robsterlini.co.uk>
    
    * Fix overflow on small screens
    
    Signed-off-by: Rob Sterlini <github@robsterlini.co.uk>

--- src/_data/redirects.js
+++ src/_data/redirects.js
@@ -1,44 +1,42 @@
-const global = require('./global.js')();
+const global = require("./global.js")();
 const links = global.links;
 const social = global.social;
 
-console.log(links);
-
 module.exports = () => ({
-  '301': {
-    '/life': links.home.link,
-    '/about': links.home.link,
-    '/cv': links.work.link,
-    '/resume': links.work.link,
-    '/curriculum-vitae': links.work.link,
-    '/site-history': links.archive.link,
-    '/journal': links.archive.link,
-    '/journal/archive': links.archive.link,
-    '/journal/past': links.archive.link,
-    '/journal/dev': links.archive.link,
-    '/journal/typography': links.archive.link,
-    '/journal/type': links.archive.link,
-    '/journal/life': links.archive.link,
-    '/fundraising': social.justgiving,
-    '/triathlon': '/2015/triathlete'
+  301: {
+    "/life": links.home.link,
+    "/about": links.home.link,
+    "/cv": links.work.link,
+    "/now": `${links.home.link}#now`,
+    "/resume": links.work.link,
+    "/curriculum-vitae": links.work.link,
+    "/site-history": links.archive.link,
+    "/journal": links.archive.link,
+    "/journal/archive": links.archive.link,
+    "/journal/past": links.archive.link,
+    "/journal/dev": links.archive.link,
+    "/journal/typography": links.archive.link,
+    "/journal/type": links.archive.link,
+    "/journal/life": links.archive.link,
+    "/fundraising": social.justgiving,
+    "/triathlon": "/2015/triathlete",
   },
-  '410': [
-    '/now'
-  ],
+  410: [],
   journal: {
-    '301': {
-      '/parkrun-changed-my-life/': '/2016/parkrun-changed-my-life/',
-      '/triathlete/': '/2015/triathlete/',
-      '/triathlon': '/2015/triathlete',
-      '/opentype-and-selection-dont-mix/': '/2014/opentype-and-selection-dont-mix/',
-      '/three-months-at-fueled': '/2013/three-months-at-fueled',
-      '/2020/a-new-start': '/2020/a-fresh-lick-of-paint',
+    301: {
+      "/parkrun-changed-my-life/": "/2016/parkrun-changed-my-life/",
+      "/triathlete/": "/2015/triathlete/",
+      "/triathlon": "/2015/triathlete",
+      "/opentype-and-selection-dont-mix/":
+        "/2014/opentype-and-selection-dont-mix/",
+      "/three-months-at-fueled": "/2013/three-months-at-fueled",
+      "/2020/a-new-start": "/2020/a-fresh-lick-of-paint",
     },
-    '410': [
-      '/good-bye-reading',
-      '/new-stuff',
-      '/the-best-city',
-      '/the-london-triathon',
+    410: [
+      "/good-bye-reading",
+      "/new-stuff",
+      "/the-best-city",
+      "/the-london-triathon",
     ],
   },
 });
